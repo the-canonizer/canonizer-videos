08359f61f6ef06eb2f9b4db2e44900ae89e2f035
BrentAllsop <brent.allsop@canonizer.com>
2020-02-06 18:45:37 -0700

    First version with partial set of canonizer html
    chapters url argument works.

---
+++ consciousness/deb_brave.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe" --allow-file-access-from-files "C:\Users\Owner\Google Drive\Canonizer\canonizer-videos\consciousness\index.html
+exit
---
+++ consciousness/deb_chrome.bat
@@ -0,0 +1,1 @@
+"c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files "C:\Users\Owner\Google Drive\Canonizer\canonizer-videos\consciousness\index.html
