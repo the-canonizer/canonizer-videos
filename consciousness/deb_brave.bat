02a06c5ae777eb500206603650418b59ad7add80
brent.allsop <brent.allsop@canonizer.com>
2021-01-27 18:32:51 -0700

    Add better selected option colors, and update to new desktop file locations for developing.
    Also, add a brave killer bat

--- consciousness/deb_brave.bat
+++ consciousness/deb_brave.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe" --allow-file-access-from-files "C:\Users\Owner\Google Drive\Canonizer\canonizer-videos\consciousness\index.html
+"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --allow-file-access-from-files "C:\Users\brent\Google Drive\Canonizer\canonizer-videos\consciousness\index.html
 exit
---
+++ consciousness/kill_brave.bat
@@ -0,0 +1,1 @@
+TASKKILL /IM brave.exe /F
